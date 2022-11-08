cf55fcb3d2997c917fdc95c1adff49be535cf926
neocream <mareyfetlock@gmail.com>
2022-11-08 22:06:42 +0100

    change theming and add an image for a banner

--- components/Header.jsx
+++ components/Header.jsx
@@ -12,25 +12,30 @@ const categories = [
 
 const Header = () => {
   return (
-    <div className='container mx-auto px-10 mb-8'>
-        <div className='border-b w-full inline-block border-blue-400 py-8'>
-            <div className='md:float-left block'>
-                <Link href={"/"}>
-                    <span className='cursor-pointer font-bold text-4xl text-white'>
-                        My Blog
-                    </span>
-                </Link>
-            </div>
-            <div className='hidden md:float-left md:contents'>
-                {categories.map((category) => (
-                    <Link key={category.slug} href={`/category/${category.slug}`}>
-                        <span className='md:float-right mt-2 align-middle text-white ml-4 font-semibold cursor-pointer'>
-                            {category.name}
+    <div className='headerwbg mb-8'>
+        <div className='container px-10 mx-auto'>
+            <div className='border-b w-full inline-block border-white-400'>
+                <div className='md:float-left block'>
+                    <Link href={"/"}>
+                        <span className='cursor-pointer font-bold text-4xl text-white'>
+                            My Blog
                         </span>
                     </Link>
-                ))}
-            </div>
+                </div>
+                <div className='hidden md:float-left md:contents'>
+                    {categories.map((category) => (
+                        <Link key={category.slug} href={`/category/${category.slug}`}>
+                            <span className='md:float-right mt-2 align-middle text-white ml-4 font-semibold cursor-pointer'>
+                                {category.name}
+                            </span>
+                        </Link>
+                    ))}
+                </div>
 
+            </div>
+        </div>
+        <div className='container mx-0 min-w-full max-h-29 overflow-hidden bannerwbg'>
+            {/* TODO: make it the gif of them walking instead */}
         </div>
     </div>
   );
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,17 +2,18 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import { PostCard, Categories, PostWidget } from '../components'
+import { getPosts } from '../services'
 
-const posts = [
-  {
-    title: "react testing", excerpt: "Learning React"
-  },
-  {
-    title: "another post", excerpt: "here is another post!"
-  }
-]
+// const posts = [
+//   {
+//     title: "react testing", excerpt: "Learning React"
+//   },
+//   {
+//     title: "another post", excerpt: "here is another post!"
+//   }
+// ]
 
-const Home: NextPage = () => {
+export default function Home({ posts }) {
   return (
     <div className="container mx-auto px-10 mb-8">
       <Head>
@@ -21,7 +22,7 @@ const Home: NextPage = () => {
       </Head>
       <div className='grid grid-cols-1 lg:grid-cols-12 gap-12'>
         <div className='lg:col-span-8 col-span-1'>
-          {posts.map((post, index) => ( <PostCard post={post} key={post.title}/> ))}
+          {posts.map((post, index) => ( <PostCard post={post.node} key={post.title}/> ))}
         </div>
         <div className='lg:col-span-4 col-span-1'>
           <div className='lg:sticky relative top-8'>
@@ -35,4 +36,10 @@ const Home: NextPage = () => {
   )
 }
 
-export default Home
+export async function getStaticProps() {
+  const posts = (await getPosts()) || [];
+
+  return {
+    props: { posts }
+  }
+}
