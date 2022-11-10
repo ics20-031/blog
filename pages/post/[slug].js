34a93791c9a29d35b37f8fda75ee5aa674d3888e
neocream <mareyfetlock@gmail.com>
2022-11-10 07:55:34 +0100

    add todo

--- pages/post/[slug].js
+++ pages/post/[slug].js
@@ -3,6 +3,7 @@ import { getPosts, getPostDetails } from '../../services';
 import { PostDetail, Categories, PostWidget, Author, Comments, CommentsForm} from '../../components';
 
 const PostDetails = ({ post }) => {
+    // TODO: somehow make it so that the title of the page matches the title of the post
   return (
     <div className='container mx-auto px-10 mb-8'>
         <div className='grid grid-cols-1 lg:grid-cols-12 gap-12'>
