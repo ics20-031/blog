1697d37ee64e1198bbe1b3b1367eb3377e03b50b
neocream <mareyfetlock@gmail.com>
2022-11-11 10:37:30 +0100

    add a hidden post feature

--- pages/index.tsx
+++ pages/index.tsx
@@ -23,7 +23,15 @@ export default function Home({ posts }) {
       </Head>
       <div className='grid grid-cols-1 lg:grid-cols-12 gap-12'>
         <div className='lg:col-span-8 col-span-1'>
-          {posts.map((post, index) => ( <PostCard post={post.node} key={post.title}/> ))}
+          {posts.map((post, index) => { 
+            console.log(post.node.hiddenPost)
+            if (!post.node.hiddenPost)
+            {
+              return (
+                  <PostCard post={post.node} key={post.title}/>
+                )
+            }
+          })}
         </div>
         <div className='lg:col-span-4 col-span-1'>
           <div className='lg:sticky relative top-8'>
--- services/index.js
+++ services/index.js
@@ -7,10 +7,11 @@ const graphqlAPI = process.env.NEXT_PUBLIC_GRAPHCMS_ENDPOINT;
 export const getPosts = async () => {
     const query = gql`
         query Authors {
-            posts {
+            posts() {
                 id
+                hiddenPost
             }
-            postsConnection {
+            postsConnection() {
                 edges {
                     node {
                         author {
@@ -32,6 +33,7 @@ export const getPosts = async () => {
                             name
                             slug
                         }
+                        hiddenPost
                     }
                 }
             }
@@ -84,6 +86,7 @@ export const getRecentPosts = async () => {
             posts(
                 orderBy: createdAt_ASC
                 last: 3 
+                where: { hiddenPost_not: true }
             ) {
                 title
                 featuredImage {
@@ -104,7 +107,7 @@ export const getSimilarPosts = async (categories, slug) => {
     const query = gql`
         query GetPostDetails($slug: String!, $categories: [String!]) {
             posts(
-                where: { slug_not: $slug, AND:{ categories_some: { slug_in: $categories }}}
+                where: { slug_not: $slug, hiddenPost_not: true, AND:{ categories_some: { slug_in: $categories }}}
                 last: 3
             ) {
                 title
