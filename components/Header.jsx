b3f0690509c476ab82b2b2d03fdb414cea297f39
neocream <mareyfetlock@gmail.com>
2022-11-13 15:24:20 +0100

    change blog name and add filter blog to header

--- components/Header.jsx
+++ components/Header.jsx
@@ -19,7 +19,6 @@ const Header = () => {
                     <div className='md:float-left block'>
                         <Link href={"/"}>
                             <span className='cursor-pointer font-bold text-4xl text-white'>
-                            {/* TODO: figure out a blog title */}
                                 Cool Blog
                             </span>
                         </Link>
