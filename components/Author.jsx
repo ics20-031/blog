3fe9b3facd7383e92713f31ce947d0c894b686f3
neocream <mareyfetlock@gmail.com>
2022-11-14 13:14:42 +0100

    add todo

--- components/Author.jsx
+++ components/Author.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 // import Image from 'next/image'
 
 const Author = ({ author }) => {
+  // TODO: change author bio into richtext and display as such
   return (
     <div className='text-center mt-20 mb-8 p-12 relative rounded-lg bg-black bg-opacity-20'>
       <div className='absolute left-0 right-0 -top-14'>
