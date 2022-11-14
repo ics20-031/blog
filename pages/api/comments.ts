02a50a5bc69424d5d4837f10ae9c43ae847e16b8
neocream <mareyfetlock@gmail.com>
2022-11-14 12:52:04 +0100

    remove broken comments code

--- pages/api/comments.ts
+++ pages/api/comments.ts
@@ -4,10 +4,10 @@
  * All files in /pages/api are mapped to /api/* and will be treated as an API endpoint
  * instead of a page.
  */
-import type { NextApiRequest, NextApiResponse } from 'next'
-import { GraphQLClient, gql } from 'graphql';
+// import type { NextApiRequest, NextApiResponse } from 'next'
+// import { GraphQLClient, gql } from 'graphql';
 
-const graphqlAPI = process.env.NEXT_PUBLIC_GRAPHCMS_ENDPOINT
+// const graphqlAPI = process.env.NEXT_PUBLIC_GRAPHCMS_ENDPOINT
 
 type Data = {
   name: string
@@ -20,10 +20,10 @@ export default function handler(
   res.status(200).json({ name: 'John Doe' })
 }
 
-export default function comments(req, res) {
-  const graphQLClient = new GraphQLClient(graphqlAPI, {
-    headers: {
-      authorization: `Bearer ${process.env}`
-    }
-  })
-}
+// export default function comments(req, res) {
+//   const graphQLClient = new GraphQLClient(graphqlAPI, {
+//     headers: {
+//       authorization: `Bearer ${process.env}`
+//     }
+//   })
+// }
