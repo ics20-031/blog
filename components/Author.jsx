df4b1808262a9f505e8462624c4a7e68463dae03
neocream <mareyfetlock@gmail.com>
2022-11-16 14:39:04 +0100

    change author bio to display as richtext

--- components/Author.jsx
+++ components/Author.jsx
@@ -3,7 +3,6 @@ import { RichText } from '@graphcms/rich-text-react-renderer';
 
 
 const Author = ({ author }) => {
-  // TODO: change author bio into richtext and display as such
   return (
     <div className='text-center mt-20 mb-8 p-12 relative rounded-lg bg-black bg-opacity-20'>
       <div className='absolute left-0 right-0 -top-14'>
