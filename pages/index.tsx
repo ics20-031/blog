bcba98e71df27eb7a09d11f601ab43f0d8b389f5
neocream <mareyfetlock@gmail.com>
2022-11-16 15:38:34 +0100

    add state hook

--- pages/index.tsx
+++ pages/index.tsx
@@ -24,6 +24,13 @@ export default function Home({ posts }) {
     if (router.isFallback) {
         return <Loader/>;
     }
+    const [thePosts, setThePosts] = useState(posts);
+
+    useEffect(() => {
+      getPosts()
+      .then((newPosts) => setThePosts(newPosts));
+    }, [])
+
   return (
     <div className="container mx-auto px-10 mb-8">
       <Head>
@@ -32,7 +39,7 @@ export default function Home({ posts }) {
       </Head>
       <div className='grid grid-cols-1 lg:grid-cols-12 gap-12'>
         <div className='lg:col-span-8 col-span-1'>
-          {posts.map((post, index) => {
+          {thePosts.map((post, index) => {
             if (!post.node.hiddenPost)
             {
               return (
@@ -43,7 +50,7 @@ export default function Home({ posts }) {
         </div>
         <div className='lg:col-span-4 col-span-1'>
           <div className='lg:sticky relative top-8'>
-            <PostWidget categories={posts.categories} slug={posts.slug} key={posts.title}/>
+            <PostWidget categories={thePosts.categories} slug={thePosts.slug} key={thePosts.title}/>
             <Categories/>
           </div>
           {/* // TODO: make a custom page work */}
